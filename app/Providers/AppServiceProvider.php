6b7248879c1ef4fcb1e3198de181fff86a37d3ad
Paulius <acuto@inbox.lt>
2018-11-21 20:11:23 +0200

    Changed string default length.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Observers\ParameterObserve;
 use App\Parameter;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -15,6 +16,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Schema::defaultStringLength(191);
+
         Parameter::observe(ParameterObserve::class);
     }
 
